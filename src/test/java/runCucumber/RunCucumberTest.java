aef1a53ba9ff424cf9daa62802155d725b636091
Lam-Pa <reyvishnyakov@gmail.com>
2020-10-19 20:42:23 +0300

    add steps folder

--- src/test/java/runCucumber/RunCucumberTest.java
+++ src/test/java/runCucumber/RunCucumberTest.java
@@ -1,12 +1,15 @@
 package runCucumber;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/main/resources/features",
         monochrome = true,
         tags = "@Test",
-        glue={"runCucumber.RunCucumberTest"}
+        glue={"steps"}
 )
 
 public class RunCucumberTest{
