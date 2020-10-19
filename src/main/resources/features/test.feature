36358db84689de4cfcb02454161d59e1ac7dccca
Lam-Pa <reyvishnyakov@gmail.com>
2020-10-19 21:02:44 +0300

    add steps folder

--- src/main/resources/features/test.feature
+++ src/main/resources/features/test.feature
@@ -1,7 +1,4 @@
-@Test
-
   Feature: something new
-
     Scenario: doing something new
       Given open browser
       Then navigate to shop
--- src/test/java/runCucumber/RunCucumberTest.java
+++ src/test/java/runCucumber/RunCucumberTest.java
@@ -7,9 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/main/resources/features",
-        monochrome = true,
-        tags = "@Test",
-        glue={"steps"}
+        glue={"online.steps"}
 )
 
 public class RunCucumberTest{
